0b0791b7d603c360f19d10f4abff9b62d7a21b96
Oleksandr <45110010+Oleksandr-Karnaushenko@users.noreply.github.com>
2024-08-29 23:44:27 +0200

    hw-2

---
+++ src/db/models/contacts.js
@@ -0,0 +1,35 @@
+// src/db/models/student.js
+
+import { model, Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      required: true,
+      enum: ['work', 'home', 'personal'],
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const ContactsCollection = model('contacts', contactsSchema);
---
+++ src/server.js
@@ -0,0 +1,67 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import env from './utils/env.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+dotenv.config();
+
+const PORT = Number(env('PORT', '3000')) || 3000;
+
+const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/contacts', async (req, res) => {
+    const students = await getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: students,
+    });
+    console.log(students);
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contact with id {contactId}!',
+      data: contact,
+    });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Route not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+export default setupServer;
